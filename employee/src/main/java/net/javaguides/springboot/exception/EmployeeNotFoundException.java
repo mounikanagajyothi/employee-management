b5481a5ee8d234e719e9672e4b7a7dcfd55241be
A. MOUNIKA REDDY <A. MOUNIKA REDDY@HP245G4>
2022-05-29 13:05:55 +0530

    employee

---
+++ employee/src/main/java/net/javaguides/springboot/exception/EmployeeNotFoundException.java
@@ -0,0 +1,12 @@
+package net.javaguides.springboot.exception;
+
+public class EmployeeNotFoundException extends Exception{
+	private long id;
+	public EmployeeNotFoundException(long id) {
+		super(String.format("Employee is not found with id:'%s'",id));
+	}
+
+}
+
+
+
---
+++ employee/src/main/java/net/javaguides/springboot/repository/employeerepository.java
@@ -0,0 +1,14 @@
+package net.javaguides.springboot.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import net.javaguides.springboot.model.employee;
+@Repository
+public interface employeerepository extends JpaRepository<employee,Long> {
+
+}
+
+
+
+
